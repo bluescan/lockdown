38751cd4636b92cd864e8a142c020ed7c960f9ff
Tristan <19311462+bluescan@users.noreply.github.com>
2020-03-04 03:22:28 -0800

    Added resource.h

---
+++ resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Lockdown.rc
+//
+#define IDR_TRAY_MENU                   101
+#define IDI_LOCKDOWN_ICON               104
+#define ID_MENU_ABOUT                   40003
+#define ID_MENU_QUIT                    40004
+#define ID__LOCK10SECONDS               40005
+#define ID__LOCKNOW                     40006
+#define ID_MENU_LOCK10SECONDS           40007
+#define ID_MENU_LOCKNOW                 40008
+#define ID_MENU_LOCK10                  40009
+
+// Next default values for new objects.
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
